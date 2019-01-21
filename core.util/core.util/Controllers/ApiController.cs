807973d6fa21e11e0989e09321b093928fc5b373
NicolYang <969502899@qq.com>
2019-01-21 23:33:34 +0800

    目录完善

---
+++ core.util/core.util/Controllers/ApiController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace core.util.Controllers
+{
+    public class ApiController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Common()
+        {
+            var s = Util.Core.Helpers.String.PinYin("你好");
+            return View();
+        }
+        public IActionResult String()
+        {
+            return View();
+        }
+    }
+}
